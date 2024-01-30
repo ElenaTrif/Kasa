568d8cf29bfe736e2b1e3e0ba9af3bc50b8b977e
ElenaTrif <140520572+ElenaTrif@users.noreply.github.com>
2024-01-30 01:49:28 +0100

    Ajout du router

---
+++ kasa-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(props) {
+    return (
+        <div className="Header">
+            <h1>Kasa Kasa</h1>
+        </div>
+    );
+}
+
+export default Header;
--- kasa-app/src/index.js
+++ kasa-app/src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./sass/index.scss";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/accomodations",
+    element: <h1>Nos locations</h1>,
+  },
+  {
+    path: "/about",
+    element: <h1>A propos</h1>,
+  },
+  {
+    path: "/404",
+    element: <h1>404</h1>,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
